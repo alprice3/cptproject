f19c825c9a49d6731fa968dfe5b184b1d7957d72
Matt <91627813+gaudyeuropa@users.noreply.github.com>
2022-02-03 13:30:41 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-function displayMenu (Vegan: boolean, Pescatarian: boolean, GF: boolean) {
-	
+function displayMenu (menuNumber: number) {
+    if (menuNumber == 1) {
+        for (let index = 0; index < 2; index++) {
+            veganReccomendation = veganCourses._pickRandom()
+            game.splash(veganReccomendation)
+            veganCourses.removeAt(veganCourses.indexOf(veganReccomendation))
+        }
+    } else if (menuNumber == 2) {
+        game.splash("")
+    } else if (menuNumber == 3) {
+        game.splash("")
+    } else {
+        game.splash("Please enter a valid input!")
+    }
 }
+let veganReccomendation = ""
+let veganCourses: string[] = []
+game.showLongText("Hi! Welcome to Viva la Special! We offer inclusive options for those with dietary restrictions!", DialogLayout.Bottom)
+let dietaryRestriction = game.askForNumber("", 1)
+veganCourses = [
+"Vegan Chicken Nuggets",
+"Tofu Power Bowl",
+"Veggie Burger with side of fries",
+"Vegan curry",
+"Red leaf"
+]
+displayMenu(dietaryRestriction)
